ce578dcd736c166240af03deca79ea693a3bca20
Leo <83018357+odduck41@users.noreply.github.com>
2024-10-13 02:34:23 +0300

    one-line main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,6 @@
 #include "../include/Updater.h"
 
 int main() {
-//    std::filesystem::path cwd = std::filesystem::current_path();
-//    std::wstring name = cwd.wstring() + L"\\..\\Winter\\Winter-1.jpg";
-//
-//    SystemParametersInfoW(
-//            SPI_SETDESKWALLPAPER,
-//            0,
-//            (PVOID)name.c_str(),
-//            SPIF_UPDATEINIFILE | SPIF_SENDCHANGE
-//    );
     circle();
     return 0;
 }
